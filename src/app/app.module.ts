16bc8120acad71496def233d69dc6e6e9dccdb6d
AlejandroVelazquez1995 <alejandro_rac_13@hotmail.com>
2022-03-31 20:06:43 +0100

    Probando a meter espacios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { HomeComponent } from './component/home/home.component';
 import { UsersComponent } from './component/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+
+
     HomeComponent,
     UsersComponent, 
   ],
